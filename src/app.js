3cea28f403d01d34190c321b2b1b51a8a9ef5f6a
Ritesh <riteshbaraptre543@gmail.com>
2023-01-06 17:23:18 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const hbs = require('hbs')
+const path = require('path')
+
+const app = express()
+app.set("view engine", "hbs")
+app.set('views', 'C:/Web Designing/Back End/Node JS/Frame Work/Express_Project/views')
+
+const partialPath = path.join(__dirname , "../partials")
+hbs.registerPartials(partialPath)
+
+
+
+//Using static files to display on Website
+// console.log(__dirname)
+app.use(express.static(path.join(__dirname,"../public")))
+
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+app.get("/about",(req,res)=>{
+    res.render("about")
+})
+app.get("/weather",(req,res)=>{
+    res.render("weather")
+})
+app.get("/contact",(req,res)=>{
+    res.send("This is Contact Page !!!")
+})
+app.get("*",(req,res)=>{
+    res.render("404error")
+})
+app.listen(8000,()=>{
+    console.log("Listening...")
+})
